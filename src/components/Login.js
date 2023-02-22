9d8b79f2a81e35bd915208081194a012c14fc3d1
dddingnan <silenceapple1027@gmail.com>
2023-02-22 13:13:49 -0500

    feat: update login page.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,28 +1,60 @@
-import * as React from "react";
+import React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import { CardActionArea, CardActions } from "@mui/material";
+import { CardActionArea } from "@mui/material";
+import Divider from "@mui/material/Divider";
 import GoogleButton from "react-google-button";
+import styled from "styled-components";
+import Avatar from "@mui/material/Avatar";
+import common from "../statics/common.png";
 
 import { signInWithGoogle } from ".././firebase";
 
+const Wraper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+`;
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Dividers = styled(Divider)`
+  && {
+    margin: 15px;
+  }
+`;
+
 export default function MultiActionAreaCard() {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <GoogleButton onClick={() => signInWithGoogle()} />
-      </CardActions>
-    </Card>
+    <Wraper>
+      <Card sx={{ maxWidth: 350 }}>
+        <CardActionArea>
+          <CardContent>
+            <Center>
+              <img src={common} style={{ width: 160, height: 85, marginBottom: 2 }} alt="Remy Sharp" loading="lazy" />
+            </Center>
+            <Typography gutterBottom variant="h5" component="div" sx={{ marginBottom: 2 }}>
+              User Login
+            </Typography>
+            <Typography variant="body1" sx={{ color: "#ee0000" }}>
+              Learning, Virtue, Piety.
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Log in to get in updates products that interests you.
+            </Typography>
+            <Dividers variant="middle" />
+            <Center>
+              <GoogleButton onClick={() => signInWithGoogle()} />
+            </Center>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Wraper>
   );
 }
